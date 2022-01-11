5a8cfe393bd0ca10d89fa9ef8b87845d4cdbe18a
Francesca Pozzolo <pozzolofrance@gmail.com>
2022-01-11 17:37:59 -0300

    Improve styles movie modal

--- src/MovieLibrary/components/MovieModal.js
+++ src/MovieLibrary/components/MovieModal.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './MovieModal.css'
-import {  Dialog, DialogContent, IconButton, Slide, Toolbar } from "@mui/material";
+import {  Dialog, DialogContent, IconButton, Rating, Slide, Toolbar } from "@mui/material";
 import { getSelectedMovie } from '../store/selectors';
 import { useSelector } from 'react-redux';
 import TMDBImage from './TMDBImage';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark, faHeart, faHeartbeat, faPlayCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const MovieModal = ({open, setOpen}) => {
 
@@ -26,27 +26,33 @@ const MovieModal = ({open, setOpen}) => {
             // TransitionComponent={Transition}
             className="modal-container"
         >
-            <Toolbar>
+            <Toolbar onClick={handleClose}>
                 <IconButton
                 edge="start"
                 color="inherit"
-                onClick={handleClose}
                 aria-label="close"
                 >
-                    <FontAwesomeIcon icon={faTimes} color='white'/>
+                    <FontAwesomeIcon icon={faTimes} color="#C21D17" />
                 </IconButton>
+                <img src='./logo.png' width="120px" alt="logo" />
             </Toolbar>
+
             <DialogContent>
-                    <div className='container-image-movie'>
-                        <div className='image-movie' style={{backgroundImage: `url(${TMDBImage(movieSelected[0].poster_path)})`}}></div>
-                    </div>
+                <div className='container-image-movie'>
+                    <div className='image-movie' style={{backgroundImage: `url(${TMDBImage(movieSelected[0].poster_path)})`}}></div>
+                </div>
                 <div className='container-movie-detail'>
                     <div className='detail-movie'>
                         <h2 className='font-title'>
-                            {movieSelected[0].title}
+                            {movieSelected[0].title} | <Rating className="rating" name="read-only" value={movieSelected[0].vote_average * 0.5} readOnly precision={0.1} size="small"/> {movieSelected[0].vote_average}
                         </h2>
                         <hr />
                         <p className='font-text'>{movieSelected[0].overview}</p>
+                        <div className='container-actions'>
+                            <FontAwesomeIcon icon={faHeart} />
+                            <FontAwesomeIcon icon={faBookmark} />
+                            <p className='font-title'>PLAY NOW <FontAwesomeIcon icon={faPlayCircle} /></p>
+                        </div>
                     </div>
                 </div> 
             </DialogContent>
